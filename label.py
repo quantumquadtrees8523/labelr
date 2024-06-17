5e10b92d14280cad1ce493155d2ccf7ac4871751
Surya Duggirala <suryaduggirala@Suryas-MBP.lan>
2024-06-17 00:40:38 -0400

    added openai api key

--- label.py
+++ label.py
@@ -4,9 +4,3 @@ from dspy.datasets.gsm8k import GSM8K, gsm8k_metric
 # Set up the LM
 turbo = dspy.OpenAI(model='gpt-3.5-turbo-instruct', max_tokens=250)
 dspy.settings.configure(lm=turbo)
-
-# Load math questions from the GSM8K dataset
-gsm8k = GSM8K()
-gsm8k_trainset, gsm8k_devset = gsm8k.train[:10], gsm8k.dev[:10]
-
-print(gsm8k_trainset)
