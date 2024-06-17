ca5175f935f9cdb208bb90eb7036035fe9e2ff57
Surya Duggirala <suryaduggirala@Suryas-MBP.lan>
2024-06-17 00:45:58 -0400

    openai working

---
+++ active_learner.py
@@ -0,0 +1,50 @@
+from utils.dspy_agent import DSPyAgent
+
+class ActiveLearner():
+    """This is the active learning agent. We aim to use this as a machine-human interface
+    that will allow us to properly label the datapoints that the machine is least confident
+    about.
+    """
+
+    def __init__(self, num_learning_iterations, task, init_training_data=[], unlabeled_data=[]):
+        """All incoming inputs must be pre-processed and cleaned.
+        """
+        self.num_learning_iterations = num_learning_iterations
+        self.task = task
+
+        self.init_training_data = init_training_data
+        self.unlabeled_data = unlabeled_data
+        self.batch_size = len(unlabeled_data) // num_learning_iterations
+
+        self.agent = DSPyAgent()
+    
+    def predict_label(self, input):
+        """Must also return a confidence score.
+        """
+        print(self.agent.forward(input))
+        return 
+
+    def gather_human_feedback(self, data_lst=[]):
+        """This will go through the list and find all elements with a low threshold score
+        and get human input on their values. It will then update the label with the human
+        input.
+        """
+        return
+    
+    def fine_tune_model(self, training_data_batch):
+        """Fine-tune the llm.
+        """
+        return
+    
+    def learn(self):
+        self.fine_tune_model(self.init_training_data)
+        for iteration in range(self.num_learning_iterations):
+            self.predict_label(self.task)
+            # pre_label_batch = self.unlabeled_data[i * self.batch_len:(i + 1) * self.batch_len]
+            # post_label_batch = [self.predict_label(inp) for inp in self.pre_label_batch]
+            # human_refined_labels = self.gather_human_feedback(post_label_batch)
+            # self.fine_tune_model(human_refined_labels)
+        return 
+    
+    def create_final_dataset(self):
+        return [self.predict_label(inp) for inp in self.unlabeled_data] + self.training_data_batch
---
+++ test.py
@@ -0,0 +1,4 @@
+from active_learner import ActiveLearner
+
+learner = ActiveLearner(1, "what color is the sky?")
+learner.learn()
