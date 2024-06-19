6dd321ff6b37a95ace60cfe21638dae9e5550b80
quantumquadtrees8523 <surya.duggirala96@gmail.com>
2024-06-18 22:47:42 -0400

    code quality: pylance works python versioning updated. beginning refactor since everything compiles

--- label.py
+++
@@ -1,6 +0,0 @@
-import dspy
-from dspy.datasets.gsm8k import GSM8K, gsm8k_metric
-
-# Set up the LM
-turbo = dspy.OpenAI(model='gpt-3.5-turbo-instruct', max_tokens=250)
-dspy.settings.configure(lm=turbo)
--- test.py
+++
@@ -1,6 +0,0 @@
-from active_learner import ActiveLearner
-from ingestion import IngestionPipeline
-
-pipeline = IngestionPipeline('datasets/data/ecommerceDataset.csv')
-# learner = ActiveLearner(1, "what color is the sky?")
-# learner.learn()
