15fe02a73f23e7bd5626abe10ef0558abd66b7a4
quantumquadtrees8523 <surya.duggirala96@gmail.com>
2024-06-19 14:46:00 -0400

    added to readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Setup
+Use this python environment
+```python3 -m venv vis-env```
+
+Activate it like this
+```source vis-env/bin/activate```
--- data_types/pre_inference_set.py
+++ data_types/pre_inference_set.py
@@ -4,8 +4,13 @@ import uuid
 class PreInferenceSet():
     def __init__(self, filename: str):
         self.input_filename = filename
-        self.dataset_df: pd.DataFrame = pd.read_csv(filename, sep="\t")
+        if self.input_filename[-4:] == '.tsv':
+            self.dataset_df: pd.DataFrame = pd.read_csv(filename, sep="\t")
+        else:
+            self.dataset_df: pd.DataFrame = pd.read_csv(filename)
         self.dataset_df['record_id'] = [uuid.uuid4() for i in range(self.dataset_df.shape[0])]
+        # Create a CSV with the record_id included.
+        self.dataset_df.to_csv(self.input_filename)
 
     # Function to get specific columns by index.
     def get_column_name(self, index: int) -> str:
